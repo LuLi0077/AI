45c6c1c884d2f92539a72a455ea6c6bc6123ff80
Lu Li <luli0077@gmail.com>
2017-01-28 13:48:03 -0800

    Pass solution_test.py

--- Sudoku/README.md
+++ Sudoku/README.md
@@ -1,29 +1,27 @@
-# Introductory Project: Diagonal Sudoku Solver
+# Diagonal Sudoku Solver
+
+Writing an AI agent to solve every diagonal sudoku puzzle. A diagonal sudoku is like a regular sudoku, except that among the two main diagonals, the numbers 1 to 9 should all appear exactly once. The approach is inspired by Peter Norvig's essay on [Solving Every Sudoku Puzzle](http://norvig.com/sudoku.html). 
+
+
+### Code
+
+* `Sudoku.ipynb` - notebook version of solution code, includes testing process and lecture notes 
+* `solutions.py` - solution code
+* `solution_test.py` - test solution by running `python solution_test.py`
+* `PySudoku.py` -  code for visualizing the solution
+* `visualize.py` - code for visualizing the solution
+
 
 ### Question 1 (Naked Twins)
 
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: 
+A: Naked twins problem states if there are two unsolved boxes in a unit and there are two digits that can only go in the same two boxes, then no others peers in that unit can have the two digits. The approach is:
+1. Use "elimination" where if a box has a value assigned, then none of the peers of this box can have this value.
+2. Search within each unit to see if there are any "twins" which are the two same digits in two boxes
+3. If there are twins, and it's possible there are multiple twins within a unit, then for each pair of twins - search each box within the unit where it contains the value of the digits in the twins and remove them. 
 
 
 ### Question 2 (Diagonal Sudoku)
 
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: 
-
-
-### Code
-
-* `Sudoku.ipynb` - final project code for the Sudoku solver, includes solution and lecture notes 
-* `solutions.py` - solution code skeleton
-* `solution_test.py` - test solution by running `python solution_test.py`.
-* `PySudoku.py` -  code for visualizing the solution.
-* `visualize.py` - code for visualizing the solution.
-
-### Visualizing
-
-To visualize your solution, please only assign values to the values_dict using the ```assign_values``` function provided in solution.py
-
-### Data
-
-The data consists of a text file of diagonal sudokus for you to solve.
+A: This just requires one addition to the regular sudoku solution - adding the two diagonal units to the unitlist and serve as constraints while performing reduce and search functions.  
--- Sudoku/solution.py
+++ Sudoku/solution.py
@@ -1,5 +1,26 @@
-assignments = []
+import numpy as np
+from collections import Counter
 
+# Encoding the board
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+  # Cross product of elements in A and elements in B
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diagonal_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'], 
+                  ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+assignments = []
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -21,10 +42,18 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
+    for unit in unitlist:
+        two_values = [values[box] for box in unit if len(values[box]) == 2]
+        twins = [twin for twin, n in Counter(two_values).items() if n == 2]
+        if len(twins) > 0:
+            for twin in twins:
+                for box in unit:
+                    if values[box] != twin:
+                        for digit in twin:
+                            if digit in values[box]:
+                                assign_value(values, box, values[box].replace(digit, ''))
+    return values
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
 
 def grid_values(grid):
     """
@@ -36,7 +65,15 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
 
 def display(values):
     """
@@ -44,19 +81,74 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1 + max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    print()
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            assign_value(values, peer, values[peer].replace(digit, ''))
+    return values
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                assign_value(values, dplaces[0], digit)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    """
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = naked_twins(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
+# Using depth-first search and propagation, try all possible values
 def search(values):
-    pass
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -67,6 +159,7 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    return search(grid_values(grid))
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
